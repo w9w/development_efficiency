6dfe619cfd6637ae46a780776200bbcc42963a66
Max Yaremchuk <38838852+w9w@users.noreply.github.com>
2021-05-03 22:54:04 +0300

    Update install_lamp_linux.sh

--- install_lamp_linux.sh
+++ install_lamp_linux.sh
@@ -2,31 +2,31 @@
 
 # This will remove Apache
 sudo service apache2 stop
-sudo apt-get purge apache2 apache2-utils apache2.2-bin 
-sudo apt remove apache2.*
-sudo apt-get autoremove
+sudo apt-get -y purge apache2 apache2-utils apache2.2-bin 
+sudo apt -y remove apache2.*
+sudo apt-get -y autoremove
 whereis apache2
 sudo rm -rf /etc/apache2
 
 # This will remove PHP
-sudo apt-get purge `dpkg -l | grep php7.2| awk '{print $2}' |tr "\n" " "`
-sudo apt-get purge php7.*
-sudo apt-get autoremove --purge
+sudo apt-get -y purge `dpkg -l | grep php7.2| awk '{print $2}' |tr "\n" " "`
+sudo apt-get -y purge php7.*
+sudo apt-get -y autoremove --purge
 whereis php
 sudo rm -rf /etc/php
 
 # This will remove MYSql
 sudo service mysql stop
-sudo apt-get remove --purge *mysql\*
-sudo apt-get remove --purge mysql-server mysql-client mysql-common -y
+sudo apt-get -y remove --purge *mysql\*
+sudo apt-get -y remove --purge mysql-server mysql-client mysql-common -y
 rm -rf /etc/mysql
-sudo apt-get autoremove
-sudo apt-get autoclean
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
 
 sudo reboot
 
 # Install MySQL & check version, start,stop & restart Apache
-sudo apt install mysql-server
+sudo apt -y install mysql-server
 sudo mysql_secure_installation
 mysql --version
 sudo service mysql start
@@ -46,18 +46,18 @@ FLUSH PRIVILEGES;
 exit
 
 # Install PHP & check version 
-sudo apt-get install software-properties-common
-sudo apt install php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
+sudo apt-get -y install software-properties-common
+sudo apt -y install php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
 php --version
-sudo apt install mlocate
+sudo apt -y install mlocate
 locate php
 whereis php
 
 
 # Install PHPmyAdmin
-sudo apt update 
-sudo apt install phpmyadmin 
-sudo apt install -y php-mbstring
-sudo apt-get install gettext
+sudo apt -y update 
+sudo apt -y install phpmyadmin 
+sudo apt -y install -y php-mbstring
+sudo apt-get -y install gettext
 
 sudo service apache2 restart 
